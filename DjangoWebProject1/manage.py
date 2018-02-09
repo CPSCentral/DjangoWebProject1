fa99db14656e696242669b0d03fb7985e2f6892e
maciasb <maciasb@cpscentral.com>
2018-02-09 11:28:29 -0500

    adas

--- DjangoWebProject1/manage.py
+++ DjangoWebProject1/manage.py
@@ -11,7 +11,7 @@ if __name__ == "__main__":
         "DJANGO_SETTINGS_MODULE",
         "DjangoWebProject1.settings"
         #dsadasda
-        #dsfafdsdsaddsaasdadadaassdasdadsadasda
+        #dsfafdsdsaddsaasdadadaassdasdadsadasdadasadsdas
     )
 
     from django.core.management import execute_from_command_line
