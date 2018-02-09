dc8f2ed1c3c6b2f1f32e333fd0a99abb75ac1fd4
maciasb <maciasb@cpscentral.com>
2018-02-09 10:45:37 -0500

    new

--- DjangoWebProject1/manage.py
+++ DjangoWebProject1/manage.py
@@ -10,6 +10,7 @@ if __name__ == "__main__":
     os.environ.setdefault(
         "DJANGO_SETTINGS_MODULE",
         "DjangoWebProject1.settings"
+        #dsadasda
     )
 
     from django.core.management import execute_from_command_line
